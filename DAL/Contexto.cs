8bd5c60cad183ab702a1b64e8e31f5757e7558cf
david-o-rosario1201 <finnnn023@gmail.com>
2024-02-12 18:28:13 -0400

    Cree el contexto

---
+++ DAL/Contexto.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Parcial1_Ap1_DavidRosario.Models;
+
+namespace Parcial1_Ap1_DavidRosario.DAL;
+
+public class Contexto : DbContext
+{
+    public Contexto(DbContextOptions<Contexto> options) : base(options)
+    {
+        
+    }
+
+    public DbSet<Metas> Metas { get; set; }
+}
