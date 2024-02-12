347d56184e767c87edb70804a9dd8da4aadb4cf4
david-o-rosario1201 <finnnn023@gmail.com>
2024-02-12 18:21:27 -0400

    Creé el modelo Metas

---
+++ Models/Metas.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Parcial1_Ap1_DavidRosario.Models;
+
+public class Metas
+{
+    [Key]
+    public int MetaId { get; set; }
+
+    [DataType(DataType.Date)]
+    public DateTime Fecha { get; set; } = DateTime.Today;
+
+    [Required(ErrorMessage = "Debe ingresar una descripción")]
+    public string Descripcion { get; set; }
+
+    [Range(1,float.MaxValue, ErrorMessage = "El monto debe ser mayor a 0")]
+    public decimal Monto { get; set; }
+}
